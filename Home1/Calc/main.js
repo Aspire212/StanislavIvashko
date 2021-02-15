5618835ae5cdbb3b7bc7b00b979ab6c5b57108ea
Stanislav <1ven970@gmail.com>
2021-02-15 17:12:37 +0300

    70%

--- Home1/Calc/main.js
+++ Home1/Calc/main.js
@@ -4,26 +4,42 @@ window.addEventListener('DOMContentLoaded', function() {
     const message = document.querySelector('.message'); //для деления на "0"!!
     const btn = document.querySelector('.keypad');
     const sign = ["+", "-", "*", "/", "="];
-    let str = '';
+    let str = '0';
+    lcd.innerHTML = str;
+    let counter = 0; //счетчик для кликов
+
     btn.addEventListener('click', function(e) {
+        counter++
+        if (counter === 1 && !isNaN(e.target.value)) {
+            str = str.slice(0, -1);
+        }
         if (e.target.value !== '=') {
             if (e.target.classList.contains('op')) {
                 //замена знака операции на новый при нажатии
                 sign.forEach(el => {
                     el === str[str.length - 1] ? str = str.slice(0, -1) : false;
                 });
-            }\
+            }
 
-            str += e.target.value;
-            lcd.innerHTML = str;
+            if (!e.target.classList.contains('keypad')) {
+                str += e.target.value;
+                lcd.innerHTML = str;
+            }
+
         }
         if (e.target.classList.contains('reset')) {
-            str = "";
+            str = "0";
+            counter = 0;
             lcd.innerHTML = str;
-        }
+        } // потанцевать с бубном
         if (e.target.classList.contains('del')) {
-            str = str.substr(0, str.length - 1);
-            lcd.innerHTML = str;
+            if (str.length !== 0) {
+                str = str.substr(0, str.length - 1);
+                lcd.innerHTML = str;
+            } else {
+                str = "0";
+                counter = 0;
+            }
         }
         if (e.target.classList.contains('equals')) {
             str = str.split(/\b/); //разбиваем строку на символы и числа
@@ -82,3 +98,5 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     }
 });
+
+// убрать возможность клика между кнопок

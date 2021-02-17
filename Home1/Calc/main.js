a0f115f5a3543b7244823b215115f1f7f59828b1
AspireMobile <1ven970@gmail.com>
2021-02-17 16:10:55 +0300

    calc

--- Home1/Calc/main.js
+++ Home1/Calc/main.js
@@ -3,7 +3,7 @@ window.addEventListener('DOMContentLoaded', function() {
     const lcd = document.querySelector('.lcd');
     const message = document.querySelector('.message'); //для деления на "0"!!
     const btn = document.querySelector('.keypad');
-    const sign = ["+", "-", "*", "/", "=", '&divide;', '&times;'];
+    const sign = ["+", "-", "*", "/", "=", "&divide;", "&times;", "."];
     let str = '0';
     lcd.innerHTML = str;
     let counter = 0; //счетчик для кликов
@@ -18,14 +18,20 @@ window.addEventListener('DOMContentLoaded', function() {
         }
         if (e.target.value !== '=') {
             if (e.target.classList.contains('op')) {
-                //замена знака операции на новый при нажатии
+                //замена знака операции на новый при нажатии         (arr[i-3] ? arr[i-3] : 100);
+
                 sign.forEach(el => {
                     el === str[str.length - 1] ? str = str.slice(0, -1) : false;
                 });
             }
-
             if (!e.target.classList.contains('keypad')) {
+              if(e.target.value === sign[7]){
+                if(!isNaN(str[str.length-2])){
+                  str += "0";
+                }
+              }
                 str += e.target.value;
+                //замена знакоа на стороне пользователя 
                 lcd.innerHTML = str.replace(sign[3], sign[5]).replace(sign[2], sign[6]);
             }
         }
@@ -35,11 +41,19 @@ window.addEventListener('DOMContentLoaded', function() {
             counter = 0;
             lcd.innerHTML = str;
         }
+        //ноль процентов !true
+        if(e.target.classList.contains('percnt') && str === '0' + e.target.value){
+          console.log(str)
+          str = "0";
+          counter = 0;
+          lcd.innerHTML = str;
+        }
         //унарный минус
         if(e.target.classList.contains('pm')){
           if(str.length === 1 && str !== '0'){
+            str = str.split(/\b/);
+            searchDot(str, sign[7]);
             str = sign[1] + str;
-            console.log(str)
           }
           else if(str[0] === sign[1]){
             str = str.substr(1, str.length-1);
@@ -65,8 +79,10 @@ window.addEventListener('DOMContentLoaded', function() {
             str = str.split(/\b/);
             //если первый символ выражения "-"
             if (str[0] === sign[1]) {
-              str.unshift(0)
+              str.unshift(0);
             }
+            searchDot(str, sign[7]);
+            searchPrc(str, '%');
             //деление на ноль
             str.forEach((el, i) => {
                 if (el === sign[3] && str[i + 1] == '0') {
@@ -79,7 +95,6 @@ window.addEventListener('DOMContentLoaded', function() {
                     }, 3000);
                 }
             });
-            searchDot(str);
             
             if (str.length < 3) {
                 str = str.join("");
@@ -88,15 +103,29 @@ window.addEventListener('DOMContentLoaded', function() {
                 calculated(str);
             }
             counter = 0;
-            lcd.innerHTML = str.join("");
+            str = str.join("");
+            lcd.innerHTML = str;
+            //str="0"//если строка не число соunter 1, если чимло str0 counter0
         }
     });
+    
+    
     //поиск точек в массиве и склеивание в одно число
-    function searchDot(arr) {
-        str.map((el, i) => {
+    function searchDot(arr, sym) {
+        arr.map((el, i) => {
             let concatEl;
-            el === "." ? concatEl = arr.splice(i - 1, 3, (arr[i - 1] + arr[i] + arr[i + 1])) : false;
+            el === sym ? concatEl = arr.splice(i - 1, 3, (arr[i - 1] + arr[i] + arr[i + 1])) : false;
         });
+    }
+    //поиск и решение процентов
+    function searchPrc(arr, sym) {
+      arr.map((el, i) => {
+        let transformEl;
+        if(el === sym){
+          let b = arr[i-3] ? parseFloat(arr[i-3]) : 1;
+         transformEl = arr.splice(i-1, 2, (parseFloat(b)/ 100 * parseFloat(arr[i-1])).toFixed(3));
+      }
+     });
     }
     //калькулятор
     function calculated(arr) {
@@ -132,10 +161,20 @@ window.addEventListener('DOMContentLoaded', function() {
             return newNum = parseFloat(num1) * parseFloat(num2);
         }
         if (op === '/') {
-            return newNum = (parseFloat(num1) / parseFloat(num2)).toFixed(4);
+            newNum = (parseFloat(num1) / parseFloat(num2));
+           return newNum === Math.floor(newNum) ?
+            Math.floor(newNum) : newNum;
         }
     }
 });
 //оптимизировать
+//добавить функцию %
+//запретить ставить несколько точек в одном числе
+//деление на 0
+// в equals доделать концовку
 
-console.log((1===1.000))
+//заменить е.target.value и e.target.classlist на переменные
+
+// перебелать безымянную ф-цию в листенере в именную
+ // сделать везде tofixed(3)
+ 

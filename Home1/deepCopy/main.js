2ab878c9feaf327f68527aade55d18e6dc493885
AspireMobile <1ven970@gmail.com>
2021-03-06 21:48:53 +0300

    format

--- Home1/deepCopy/main.js
+++ Home1/deepCopy/main.js
@@ -2,33 +2,27 @@
 
 let bcr = {a: {b:2, d: {e: 18, g: [1, 2, 3]}}, c:4, f: [1, 2, [18, 22]]}
 
-function deepCopy(obj){
+
+
+function deepCopy(obj) {
   let newObj = {};
-  
   const copyArr = (arr) => {
     let newArr = [];
-    arr.forEach((el, i) =>
-      el instanceof Object ?
-      newArr[i] = copyArr(el) :
-      newArr[i] = el
-    );
+    arr.forEach((el, i) => el instanceof Object ? newArr[i] = copyArr(el) : newArr[i] = el);
     return newArr;
   }
-  
-  for(let key in obj){
-    if(obj[key] instanceof Object && !Array.isArray(obj[key])){
-      
+  for (let key in obj) {
+    if (obj[key] instanceof Object && !Array.isArray(obj[key])) {
       newObj[key] = deepCopy(obj[key]);
-    }
-    else if(obj[key] instanceof Object && Array.isArray(obj[key])){
-     newObj[key] =  copyArr(obj[key]);
-    }
-    else{
+    } else if (obj[key] instanceof Object && Array.isArray(obj[key])) {
+      newObj[key] = copyArr(obj[key]);
+    } else {
       newObj[key] = obj[key];
     }
   }
   return newObj;
 }
+
 
 let nO = deepCopy(bcr);
 nO.f[2][0] = 88;

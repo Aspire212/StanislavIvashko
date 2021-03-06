ed25187fb113f86fcad1cb0dcf6839a3b1ef3b6c
AspireMobile <1ven970@gmail.com>
2021-03-06 21:38:46 +0300

    deepCopy

---
+++ Home1/deepCopy/main.js
@@ -0,0 +1,38 @@
+'use strict'
+
+let bcr = {a: {b:2, d: {e: 18, g: [1, 2, 3]}}, c:4, f: [1, 2, [18, 22]]}
+
+function deepCopy(obj){
+  let newObj = {};
+  
+  const copyArr = (arr) => {
+    let newArr = [];
+    arr.forEach((el, i) =>
+      el instanceof Object ?
+      newArr[i] = copyArr(el) :
+      newArr[i] = el
+    );
+    return newArr;
+  }
+  
+  for(let key in obj){
+    if(obj[key] instanceof Object && !Array.isArray(obj[key])){
+      
+      newObj[key] = deepCopy(obj[key]);
+    }
+    else if(obj[key] instanceof Object && Array.isArray(obj[key])){
+     newObj[key] =  copyArr(obj[key]);
+    }
+    else{
+      newObj[key] = obj[key];
+    }
+  }
+  return newObj;
+}
+
+let nO = deepCopy(bcr);
+nO.f[2][0] = 88;
+nO.a.d.e = 1000;
+nO.a.d.g[1] = "tratata";
+console.log(bcr, 'bcr');
+console.log(nO);

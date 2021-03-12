8c42052b2e7b97aa4af58bcff483233f81509b26
Stanislau <1ven970@gmail.com>
2021-03-12 22:36:51 +0300

    ret

--- Home1/calc Foo/main.js
+++ Home1/calc Foo/main.js
@@ -1,98 +1,45 @@
 'use strict'
-let reg = "2+2+(3+1+(2-1*(5)))"
-const sign = ["+", "-", "*", "/", "=", "&divide;", "&times;", "."];
-   reg = reg.split("").filter(el => el !==" ").join("");
+let reg = '2 * (-3 + 1)'; //=> -4
 
-console.log(reg)
-//функция чтобы добраться до самых глубоких скоок
-function brekit(str){
-  let myStr = str;
-  let sub = str;
-  if(!sub.substr(1).includes("(")){
-    let newStr = sub.slice(1, sub.indexOf(")"));
-    let oldStr = sub.slice(0, sub.indexOf(")") + 1);
-   return reg.replace(oldStr, calculated(newStr));
-  }
-  else{
-    return brekit(sub.substr(1));
-  }
+function calc(str) {
+    str = str.split(/\b/);
+
+    return str
 }
-reg = brekit(reg)
-reg = brekit(reg)
-reg = brekit(reg)
+console.log(calc(reg))
 
 
 
-
-
-/*function repl(str) {
-   if(!str.includes("(")){
-     return str 
-   }
-   else{
-     str = brekit(reg)
-     return repl(str.substr(1))
-    
+/*
+function zeros(arr, sym) {
+        //деление на ноль
+        arr.forEach((el, i) => {
+            if (el === sym && (arr[i + 1] === '0' || arr[i + 1] === '0' && arr[i + 2] === '%')) {
+                arr = '0';
+                lcd.innerHTML = arr;
+                counter = 0;
+                message.innerHTML = "На ноль делить нельзя!";
+                setTimeout(() => {
+                    message.innerHTML = "";
+                    lcd.innerHTML = arr;
+                }, 3000);
+            }
+        });
     }
-    
-    
-    //вставить все функцми
-    
-    
-}
-console.log(repl(reg))*/
-
-    //калькулятор
-    function calculated(arr) {
-      arr = arr.split("");
-      arr.includes(".") && searchDot(arr, sign[7])
-      operation(arr, sign[3], sign[2]); //сразу "* и /"
-      operation(arr, sign[1], sign[0]); //после "- и +"
-      return arr.join("")
-
-    }
-    //математическая логика для правильной последовательности операций
-    function operation(arr, op1, op2 = op1) {
-      console.log(arr)
-      arr.forEach(el => (el !== op1 || el !== op2) ? mathLogic(arr, op1, op2) : false);
-    }
-    //замена 3-х подряд идущих знаков в массиве на их решение
-    function mathLogic(arr, op1, op2 = op1) {
-      if (arr.length < 2) {
-        return arr.join("");
-      }
-      let replaceEl;
-      arr.forEach((el, i) => {
-        if (arr[i] === op1 || arr[i] === op2) {
-          replaceEl = arr.splice(i - 1, 3, equals(arr[i - 1], arr[i], arr[i + 1]));
-        }
-      })
-    }
+    //поиск точек в массиве и склеивание в одно число
     function searchDot(arr, sym) {
-      arr.map((el, i) => {
-        el === sym ? el = arr.splice(i - 1, 3, (arr[i - 1] + arr[i] + arr[i + 1])) : false;
-      });
+        arr.map((el, i) => {
+            el === sym ? el = arr.splice(i - 1, 3, (arr[i - 1] + arr[i] + arr[i + 1])) : false;
+        });
     }
-    //решение выражения
-    function equals(num1, op, num2) {
-      if(num2 === "(") return num2
-      let newNum;
-      if (op === '+') {
-        return newNum = parseFloat(num1) + parseFloat(num2);
-      }
-      if (op === '-') {
-        return newNum = parseFloat(num1) - parseFloat(num2);
-      }
-      if (op === '*') {
-        return newNum = parseFloat(num1) * parseFloat(num2);
-      }
-      if (op === '/') {
-        newNum = (parseFloat(num1) / parseFloat(num2));
-        if (newNum === Infinity) {
-          return newNum = 0;
-        } else {
-          return newNum === Math.floor(newNum) ? Math.floor(newNum) : newNum.toFixed(3);
-        }
-      }
+    //поиск и решение процентов
+    function searchPrc(arr, sym) {
+        arr.map((el, i) => {
+            let transformEl;
+            if (el === sym) {
+                let b = (arr[i - 2] === sign[0] || arr[i - 2] === sign[1]) ? arr[i - 3] : 1;
+                transformEl = arr.splice(i - 1, 2, (parseFloat(b) / 100 * parseFloat(arr[i - 1])).toFixed(3));
+            }
+        });
     }
-//console.log(calculated(reg))
+*/
